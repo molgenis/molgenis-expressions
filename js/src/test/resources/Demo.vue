746c18ad4892c9e938eb20a3fd11fa974868b1d5
Fleur Kelpin <f.kelpin@umcg.nl>
2021-05-28 13:24:30 +0200

    test: list variables in Demo.vue

--- js/src/test/resources/Demo.vue
+++ js/src/test/resources/Demo.vue
@@ -2,15 +2,23 @@
   <div>
     <h1>Evaluate demo</h1>
     <form>
-      Expression:<input type="text" v-model="expression"/><br/>
-      Data: <textarea v-model="data"/>
+      <h2>Expression</h2>
+      <input type="text" v-model="expression"/><br/>
+      <h2>Variables</h2>
+      <ul>
+        <li v-for="variable in variables" :key="variable"><pre>{{variable}}</pre></li>
+      </ul>
+      <h2>Data</h2>
+      <textarea v-model="data" rows="4"/>
     </form>
+  
+    <h2>Result</h2>
     <pre> {{ result }} </pre>
   </div>
 </template>
 
 <script>
-  import {evaluate} from "../../../target/scala-2.13/molgenis-expressions-opt/main.mjs"
+  import {evaluate, variableNames} from "../../../target/scala-2.13/molgenis-expressions-opt/main.mjs"
   window.evaluate = evaluate
   export default {
     data () {
@@ -26,6 +34,14 @@
         }
         catch (error) {
           return error
+        }
+      },
+      variables () {
+        try {
+          return variableNames(this.expression)
+        }
+        catch (error) {
+          return []
         }
       }
     }
