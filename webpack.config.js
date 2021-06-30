baed311490d1b1a04d4c5b539b3e65fa311e86d7
Fleur Kelpin <f.kelpin@umcg.nl>
2021-06-30 12:54:39 +0200

    fix: no es6 features in webpack
    Makes it work on IE11

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,22 @@ module.exports = {
     main: './main.js'
   },
   output: {
+    environment: {
+      // The environment supports arrow functions ('() => { ... }').
+      arrowFunction: false,
+      // The environment supports BigInt as literal (123n).
+      bigIntLiteral: false,
+      // The environment supports const and let for variable declarations.
+      const: true,
+      // The environment supports destructuring ('{ a, b } = obj').
+      destructuring: false,
+      // The environment supports an async import() function to import EcmaScript modules.
+      dynamicImport: false,
+      // The environment supports 'for of' iteration ('for (const x of array) { ... }').
+      forOf: false,
+      // The environment supports ECMAScript Module syntax to import ECMAScript modules (import ... from '...').
+      module: false,
+    },
     library: {
       name: 'Expressions',
       type: 'umd'
